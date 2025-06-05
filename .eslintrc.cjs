20bf218772dae92ff399b9781498c988c2830da7
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-05 12:46:37 +0300

    set up enviroment

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+// .eslintrc.cjs
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
