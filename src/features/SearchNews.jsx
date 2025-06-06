1305ccadecb5f2d824d85957e25dd93f5c80d785
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-06 18:50:39 +0300

    update search input

--- src/features/SearchNews.jsx
+++ src/features/SearchNews.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 export default function SearchNews() {
   return (
-    <form>
+    <form className="flex items-center relative">
       <input
-        className="bg-amber-50 rounded-full w-44 h-7 text-stone-900 px-4 py-0.25 md:w-96 focus:outline-none focus:w-56 md:focus:w-96"
+        className="md:w-96 w-56 border-2 rounded-full px-4 h-10 border-stone-200"
         type="text"
-        placeholder="search for news"
+        placeholder="Search tech news..."
       />
+      <button
+        className="font-light text-stone-400 absolute md:left-[350px] left-[190px]"
+        type="submit"
+      >
+        <i class="fas fa-search"></i>
+      </button>
     </form>
   );
 }
