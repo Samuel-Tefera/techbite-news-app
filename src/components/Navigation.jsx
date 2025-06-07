82d6c8cabf3153f029f5d69fd0477366232c5ea4
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-07 18:01:11 +0300

    make responsive navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,12 +4,12 @@ import SearchNews from '../features/SearchNews';
 
 export default function Navigation() {
   return (
-    <nav className="flex flex-col gap-2 sm:flex-row text-center sm:text-left items-center justify-between px-16 py-4 shadow-md">
+    <nav className="flex w-full gap-2 items-center justify-between sm:px-16 px-2 py-4 shadow-md">
       <div>
         <Link className="text-2xl font-semibold" to="#">
           <span className="text-blue-600">Tech</span>Bite
         </Link>
-        <p className="font-light">Stay Sharp With Daily Tech Headlines!</p>
+        <p className="font-light sm:block hidden">Stay Sharp With Daily Tech Headlines!</p>
       </div>
       <SearchNews />
     </nav>
