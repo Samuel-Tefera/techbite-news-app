8b3c62e39cd65748d36830b65b2df23c4bbd01b2
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-08 21:03:47 +0300

    make it fixed on top and give background colour

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ import SearchNews from '../features/SearchNews';
 
 export default function Navigation() {
   return (
-    <nav className="flex w-full gap-2 items-center justify-between sm:px-16 px-2 py-4 shadow-md">
+    <nav className="flex bg-white w-full fixed z-10 top-0 left-0 gap-2 items-center justify-between sm:px-16 px-2 py-4 shadow-md">
       <div>
         <Link className="text-2xl font-semibold" to="#">
           <span className="text-blue-600">Tech</span>Bite
