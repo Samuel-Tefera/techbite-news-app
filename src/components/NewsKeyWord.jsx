85772b22a799f1d814307e566cb2eea8bc17a3bd
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-09 17:48:18 +0300

    give style for keywords

--- src/components/NewsKeyWord.jsx
+++ src/components/NewsKeyWord.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function NewsKeyWord({ keywords }) {
   return (
-    <ul>
+    <ul className="flex gap-2 mb-4 mt-4">
       {keywords.map((keyword) => (
-        <li>{keyword}</li>
+        <li className="text-xs bg-blue-600 px-1 py-0.5 text-white rounded-md">{keyword}</li>
       ))}
     </ul>
   );
