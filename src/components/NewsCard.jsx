2b6fb9b1b3c75ba1746cda86301e817f8b1db628
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-09 12:42:26 +0300

    display news informations

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import NewsKeyWord from './NewsKeyWord';
 
-export default function NewsCard() {
-  return <div>NewsCard</div>;
+export default function NewsCard({ news }) {
+  const {
+    title,
+    keywords,
+    description,
+    // content,
+    pubDate,
+    image_url,
+    source_id,
+    link,
+  } = news;
+  return (
+    <div>
+      <img src={image_url} alt={title} />
+      <NewsKeyWord keywords={keywords} />
+      <a href={image_url}>
+        <h2>{title}</h2>
+      </a>
+      <a href={link}>
+        <h2>{description}</h2>
+      </a>
+      <div>
+        <div>
+          <p>⌚</p>
+          <p>{pubDate}</p>
+        </div>
+        <p>{source_id}</p>
+      </div>
+    </div>
+  );
 }
