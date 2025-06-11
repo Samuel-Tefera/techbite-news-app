1afd30c6c93185dcfbe6a70784166cfe6953769d
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-11 19:34:06 +0300

    remove content from the card

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,31 +3,47 @@ import NewsKeyWord from './NewsKeyWord';
 import { formatTimeAgo } from '../utils/helper';
 
 export default function NewsCard({ news }) {
-  const { title, keywords, description, content, pubDate, image_url, source_id, link } = news;
+  const {
+    title,
+    keywords,
+    description,
+    pubDate,
+    image_url,
+    source_id,
+    link,
+  } = news;
   return (
     <div className="shadow-md rounded-b-md">
-      <img className="w-full rounded-t-md" src={image_url} alt={title} />
+      <img
+        className="w-full rounded-t-md"
+        src={image_url}
+        alt={title}
+      />
       <div className="pb-4 px-2">
         <NewsKeyWord keywords={keywords} />
         <h2 className="font-semibold text-stone-800 text-[1.1rem] mb-2">
           <a href={link}>{title}</a>
         </h2>
-        <h3 className="text-stone-700 text-sm">
+        <h3 className="text-stone-700 text-sm mb-4">
           <a href={link}>{description}</a>
         </h3>
-        <p className="text-sm mt-2 mb-4 text-stone-600">{content}</p>
         <a
-          href="#"
+          href={link}
           className="border border-[#2563eb] py-2 px-4 bg-[#2563eb] rounded-md text-white font-medium text-sm"
         >
-          Read More Story <i class="fas fa-external-link-alt" />
+          Read More Story{' '}
+          <i class="fas fa-external-link-alt" />
         </a>
         <div className="flex justify-between mt-4 text-xs">
           <div className="flex items-center gap-1.5 text-stone-600">
             <i class="far fa-clock text-shadow-stone-600" />
-            <p className="text-stone-600">{formatTimeAgo(pubDate)}</p>
+            <p className="text-stone-600">
+              {formatTimeAgo(pubDate)}
+            </p>
           </div>
-          <p className="bg-stone-200 px-1 py-0.5 rounded-md text-stone-700">{source_id}</p>
+          <p className="bg-stone-200 px-1 py-0.5 rounded-md text-stone-700">
+            {source_id}
+          </p>
         </div>
       </div>
     </div>
