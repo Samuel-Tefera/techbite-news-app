0a2fe23f4515ec5cf05682a15d561c4227315d4c
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-13 17:43:31 +0300

    fetch news and render it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../components/Navigation';
 import Navigation from '../components/Navigation';
 import SideBar from '../components/SideBar';
-import NewsCard from '../components/NewsCard';
-
-const newsDataRequest = await fetch('../../mock/sample-news.json');
-const newsData = await newsDataRequest.json();
-const newsItems = newsData.results;
+import Footer from '../components/Footer';
+import LoadingSpinner from '../components/LoadingSpinner';
+import NewsList from '../components/News/NewsList';
+import { getLatestNews } from '../services/api-newsData';
 
 export default function Home() {
+  const [newsData, setNewsData] = useState(null);
+
+  useEffect(() => {
+    async function fetchNews() {
+      const data = await getLatestNews();
+      setNewsData(data.newsData);
+    }
+    fetchNews();
+  }, []);
+
   return (
     <>
       <header>
         <Navigation />
       </header>
-      <div className="sm:mt-23 mt-18">
-        <div>
+      <main className="grid md:grid-cols-5 sm:mt-23 mt-18">
+        <div className="col-span-1">
           <SideBar />
         </div>
-        <div className="md:ml-[20%] w-max px-10 md:grid grid-cols-3 gap-4">
-          {newsItems.map((news) => (
-            <NewsCard news={news} />
-          ))}
+        <div className="sm:px-10 px-6 col-span-4 h-screen overflow-y-scroll minimal-scrollbar">
+          <div className="py-3 sm:py-6">
+            <h2 className="py-3 text-xl sm:text-2xl border-b border-stone-200 font-semibold">
+              Latest Tech News
+            </h2>
+          </div>
+          {!newsData && (
+            <div>
+              <LoadingSpinner />
+            </div>
+          )}
+          {newsData && <NewsList newsData={newsData} />}
         </div>
-      </div>
+      </main>
+      <Footer />
     </>
   );
 }
