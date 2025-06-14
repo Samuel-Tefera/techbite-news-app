de90d0fffb58f99e8a675f5ffa5d2835d3fa258f
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-14 17:05:12 +0300

    update it to accept messege prop

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ messege }) {
   return (
     <div className="flex flex-col items-center p-11 justify-center">
       <div className="spinner mb-2"></div>
-      <p className="text-sm text-stone-500">Loading more news...</p>
+      <p className="text-sm text-stone-500">{messege}</p>
     </div>
   );
 }
