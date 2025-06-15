41674e50a73de7045512edda9809fe4f1e66a473
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-15 16:45:41 +0300

    update input field size

--- src/features/SearchNews.jsx
+++ src/features/SearchNews.jsx
@@ -4,7 +4,7 @@ export default function SearchNews() {
   return (
     <form className="flex items-center relative">
       <input
-        className="md:w-96 w-56 border-2 rounded-full px-4 h-10 border-stone-200"
+        className="w-56 text-sm md:text-lg md:w-96 border-2 rounded-full px-4 h-8 md:h-10 border-stone-200"
         type="text"
         placeholder="Search tech news..."
       />
@@ -12,7 +12,7 @@ export default function SearchNews() {
         className="font-light text-stone-400 absolute md:left-[350px] left-[190px]"
         type="submit"
       >
-        <i class="fas fa-search"></i>
+        <i className="fas fa-search" />
       </button>
     </form>
   );
