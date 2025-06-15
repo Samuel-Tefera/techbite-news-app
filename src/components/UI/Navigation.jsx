7df2d2d637b719f046017827add7639f77364374
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-15 16:46:19 +0300

    update size of the logo

--- src/components/UI/Navigation.jsx
+++ src/components/UI/Navigation.jsx
@@ -6,10 +6,10 @@ export default function Navigation() {
   return (
     <nav className="flex bg-white w-full fixed z-10 top-0 left-0 gap-2 items-center justify-between sm:px-16 px-2 py-4 shadow-md">
       <div>
-        <Link className="text-2xl font-semibold" to="#">
+        <Link className="text-xl md:text-3xl sm:text-2xl font-semibold" to="#">
           <span className="text-blue-600">Tech</span>Bite
         </Link>
-        <p className="font-light sm:block hidden">
+        <p className="font-light lg:block hidden">
           Stay Sharp With Daily Tech Headlines!
         </p>
       </div>
