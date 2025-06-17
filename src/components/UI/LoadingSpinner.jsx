bbe7655b61041c80ca3179e8f44adabc1dfe7fe8
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-17 15:04:24 +0300

    fix typo mistake

--- src/components/UI/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function LoadingSpinner({ messege }) {
+export default function LoadingSpinner({ message }) {
   return (
     <div className="flex flex-col items-center p-11 justify-center">
       <div className="spinner mb-2 border-t-blue-600 dark:border-t-blue-400 border-l-blue-600 dark:border-l-blue-400"></div>
-      <p className="text-sm text-gray-600 dark:text-gray-400">{messege}</p>
+      <p className="text-sm text-gray-600 dark:text-gray-400">{message}</p>
     </div>
   );
 }
