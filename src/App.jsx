99334c99a0426e698673613d5d453fb4b734dfb9
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-18 20:35:08 +0300

    wrap app with search provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Home from './pages/Home';
+import { SearchProvider } from './context/SearchContext';
 
 const router = createBrowserRouter([
   {
@@ -14,9 +15,11 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-500">
-        <RouterProvider router={router} />
-      </div>
+      <SearchProvider>
+        <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-500">
+          <RouterProvider router={router} />
+        </div>
+      </SearchProvider>
     </ThemeProvider>
   );
 }
