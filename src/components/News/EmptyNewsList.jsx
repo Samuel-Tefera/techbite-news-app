e2de50a8994c51bc2f322dc69f46c1b4123d9f96
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-21 18:26:27 +0300

    edit messege and title texts

--- src/components/News/EmptyNewsList.jsx
+++ src/components/News/EmptyNewsList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export default function EmptyNewsList({ message }) {
+export default function EmptyNewsList() {
   return (
-    <div className="flex flex-col items-center p-4 sm:p-10 justify-center">
-      <h2 className="text-xl sm:text-2xl text-gray-700 dark:text-gray-300">
-        Faild to Fetch News
+    <div className="flex flex-col items-center p-4 sm:p-10 justify-center mt-4">
+      <h2 className="text-xl sm:text-2xl text-gray-700 dark:text-gray-300 mb-2">
+        We Looked Everywhere...
       </h2>
       <p className="text-sm sm:text-xl text-gray-600 dark:text-gray-400">
-        {message}
+        But there’s nothing that matches your search. Try more general terms or
+        explore other trending topics.
       </p>
     </div>
   );
