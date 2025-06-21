509a9b3c022860bbe956542e93cd6f22c0650e7a
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-21 18:31:50 +0300

    pass query prop to news list componenet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import Navigation from '../components/UI/Navigation';
 import SideBar from '../components/UI/SideBar';
 import Footer from '../components/UI/Footer';
 import NewsList from '../components/News/NewsList';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('search');
+
   return (
     <>
       <header className="bg-white dark:bg-gray-800 shadow-sm">
@@ -13,7 +17,7 @@ export default function Home() {
         <div className="col-span-1 bg-white dark:bg-gray-800 shadow-sm">
           <SideBar />
         </div>
-        <NewsList />
+        <NewsList query={query} />
       </main>
       <Footer />
     </>
