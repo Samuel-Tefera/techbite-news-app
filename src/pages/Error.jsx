5ed414bef5103e3cbd030edd46be0477322e3f81
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-22 14:27:49 +0300

    add error boundary class

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { useNavigate, useRouteError } from 'react-router-dom';
 
-export default function Error() {
-  const error = useRouteError();
+export default function Error(props) {
+  const routeError = useRouteError();
   const navigate = useNavigate();
+  const error = props.error || routeError;
+  console.log(error);
+
   let title = 'Oops';
   let message = 'Something went wrong.';
-  if (error?.status === 404) {
+
+  if (
+    error?.message?.includes('NetworkError') ||
+    error?.message?.includes('Failed to fetch')
+  ) {
+    title = 'Network Issue';
+    message =
+      'We’re having trouble connecting to the server. Please check your internet connection and try again.';
+  } else if (error?.status === 404) {
     title = 'Page not found.';
     message = `The page you're looking for doesn’t exist or the link might be broken.
-              Double-check the URL or head back to the homepage.`;
+            Double-check the URL or head back to the homepage.`;
   } else if (error?.status === 429) {
     title = 'We’re Busy at the Moment';
     message = `Our news source is receiving a lot of traffic right now.
-              Please hang tight — TechBite will refresh shortly with the latest headlines.`;
+            Please hang tight — TechBite will refresh shortly with the latest headlines.`;
   }
+
   return (
     <div className="px-6 sm:px-16 md:px-36 py-28">
       <h1 className="text-xl sm:text-2xl text-gray-700 dark:text-gray-300 mb-2">
@@ -35,4 +47,24 @@ export default function Error() {
       </div>
     </div>
   );
+}
+
+// This is the class component ErrorBoundary logic
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <Error error={this.state.error} />;
+    }
+
+    return this.props.children;
+  }
 }
